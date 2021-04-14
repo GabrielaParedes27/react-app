cd16e522b10988c55253e5ad198622aea06081d6
GabrielaParedes27 <78928248+GabrielaParedes27@users.noreply.github.com>
2021-04-13 22:07:17 -0500

    added return null

--- src/Weather.js
+++ src/Weather.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import axios from 'axios';
-
-
 export default function Weather(){
-    function handleResponse(response) {
-        alert(`The weather in New York is ${response.data.main.temp}°C`);
-    }
-    let apiKey = "816a63a33af440332c05784e3d9896ea";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(handleResponse);
+  function handleResponse(response) {
+    alert(`The weather in New York is ${response.data.main.temp}°C`);
+  }
+  let apiKey = "816a63a33af440332c05784e3d9896ea";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(handleResponse);
+  
+  return null;
 }
